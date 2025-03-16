fd5e35c3edeb23268f71e29253c501836f27df24
Oluwabusola Marcus <marcusbusola@gmail.com>
2025-03-16 11:59:36 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -281,7 +281,7 @@ export default function Home() {
               href="/contact"
               className="light-border bg-[#EDE7DE] text-black px-6 py-2 rounded-full text-sm hover:bg-gray-200 transition-all duration-300 ease-in-out relative overflow-hidden"
             >
-              Let's Talk →
+              Let"s Talk →
             </a>
           </div>
         </div>
