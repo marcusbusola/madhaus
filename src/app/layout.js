c952502dc8b95a318409a7487537e510f3b94c29
Oluwabusola Marcus <marcusbusola@gmail.com>
2025-04-20 12:26:57 -0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,15 +26,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${manrope.variable} antialiased`}>
-        <!-- Google tag (gtag.js) -->
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-FSEDXP0GJ6"></script>
-          <script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FSEDXP0GJ6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-          
             gtag('config', 'G-FSEDXP0GJ6');
-          </script>
+          `}
+        </Script>
         {children}
       </body>
     </html>
